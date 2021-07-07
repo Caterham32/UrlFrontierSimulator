211279137dbbc5203381133bad7ada210f103fff
Daniel <42212148+Caterham32@users.noreply.github.com>
2021-07-07 20:28:32 +0200

    added front queues + renaming

--- Crawler/main.py
+++ Crawler/main.py
@@ -7,6 +7,15 @@ config = yaml.safe_load(open("config.yml"))
 InitialUrlCount = config["numberUrl"]
 InitialServerCount = config["numberServer"]
 intervall = config["intervall"]
+numberOfFrontQueues = config["numberOfFrontQueues"]
+
+urlNumber = 0
+latencyMin = 10
+latencyMax = 1000
+updateTimeMin = 10
+updateTimeMax = 1000
+
+updateTimeIntervall = (updateTimeMax-updateTimeMin)/numberOfFrontQueues
 
 class UrlModel:
     def __init__(self, identifier, server, updateTime : int):
@@ -15,9 +24,9 @@ class UrlModel:
         self.UpdateTime = updateTime
 
     def print(self):
-        print("URL: " + str(self.Identifier))
+        print("URL number: " + str(self.Identifier))
         self.Server.print()
-        print("UpdateTime: " + str(self.UpdateTime))
+        print("Url UpdateTime: " + str(self.UpdateTime))
 
 class Server:
     def __init__(self, identifier, latency : int):
@@ -25,41 +34,70 @@ class Server:
         self.Latency = latency
 
     def print(self):
-        print("Identifier: " + str(self.Identifier))
-        print("Latency: " + str(self.Latency))
+        print("Server Identifier: " + str(self.Identifier))
+        print("Server Latency: " + str(self.Latency))
 
 def generateServer(count : int):
+    global latencyMin, latencyMax
     list = []
     for i in range(count):
-        latency = random.randint(10, 1000)
+        latency = random.randint(latencyMin, latencyMax)
         list.append(Server(i,latency))
     return list
 
 def generateUrls(queue, count : int, serverList):
-    maxIndex = len(serverList)-1
+    global urlNumber, updateTimeMin, updateTimeMax
+    maxIndex = len(serverList) - 1
     for i in range(count):
+        # used to choose server
         index = random.randint(0, maxIndex)
-        updatetime  = random.randint(10, 1000)
-        queue.put(UrlModel(i,serverList[index],updatetime))
+        updatetime  = random.randint(updateTimeMin, updateTimeMax)
+        queue.put(UrlModel(urlNumber, serverList[index],updatetime))
+        urlNumber += 1
     return queue
 
-def generateData(queue, serverCount: int, urlCount : int):
-    serverlist = generateServer(serverCount)
-    return generateUrls(queue, urlCount,serverlist)
-    
+def prioritization(urlQueue, frontQueues):
+    global updateTimeIntervall
+    while not urlQueue.empty():
+        item = urlQueue.get()
+        index = int((item.UpdateTime - updateTimeMin)//updateTimeIntervall)
+        frontQueues[index].put(item)
+    return frontQueues
+
+
+
+# list of timestamps    
 times = [1]
 
-urlList = queue.Queue()
+# initial queue
+urlQueue = queue.Queue()
 
-urlList = generateData(urlList, InitialServerCount,InitialUrlCount)
+# initial frontQueue
+frontQueues = []
+for i in range(numberOfFrontQueues):
+    frontQueues.append(queue.Queue())
 
-#for timestamp in times:
-    # generateData()
+# generate initial data
+serverlist = generateServer(InitialServerCount)
+generateUrls(urlQueue, InitialUrlCount, serverlist)
 
+for timestamp in times:
 
-while not urlList.empty():
-    item = urlList.get()
-    item.print()
+    # generate new data at timestamp
+    urlCount = random.randint(10, 100)
+    urlQueue = generateUrls(urlQueue, urlCount, serverlist)
+    frontQueues = prioritization(urlQueue,frontQueues)
+
+i = 0
+
+for frontQueue in frontQueues:
+    print("frontQueue " + str(i) + " :" )
+    while not frontQueue.empty():
+        item = frontQueue.get()
+        item.print()
+    print ("-----------------------------------------------------")
+    i += 1
+
 
 
    
